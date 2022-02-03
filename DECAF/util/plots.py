38b0a64b42a37a2485a6f3044264faa7cc8c8739
Zoë Prins <zoe.prins@hotmail.com>
2022-02-03 09:46:01 +0100

    Ran models adult dataset second dag and adjusted plots.py to work with std

--- DECAF/util/plots.py
+++ DECAF/util/plots.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 def plot_errorbar(betas, metric_name, results):
     fig = plt.figure()
     for (algo, result) in results.items():
-        # metric, low, high = list(zip(*result))
-        # metric = list(metric)
-        # yerr = [list(low), list(high)]
-        plt.errorbar(betas, result, yerr=None,  capsize=4, capthick=1, label=algo)
+        metric, std = list(zip(*result))
+        metric = list(metric)
+        yerr = list(std)
+        plt.errorbar(betas, metric, yerr=yerr,  capsize=4, capthick=1, label=algo)
     plt.xlabel('Bias ' + r'$\beta$')   
     plt.ylabel(metric_name)
     plt.legend()
